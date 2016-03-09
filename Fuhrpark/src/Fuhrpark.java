c7d413b4c752ce9741ac221e93d37ebc4ddcf90e
uidp3755 <uidp3755@BHL5B1FG.cw01.contiwan.com>
2016-03-09 16:14:06 +0100

    System.out.println("guge ma hallo was geht?, Tes2\n");}

--- Fuhrpark/src/Fuhrpark.java
+++ Fuhrpark/src/Fuhrpark.java
@@ -1,5 +1,5 @@
 public class Fuhrpark {
 
 	public static void main(String[] args) {
-		System.out.println("guge ma hallo was gdeht?, Tes2\n");}
+		System.out.println("guge ma hallo was geht?, Tes2\n");}
 }
