2cd7ffc3d39a356ff50fb38d5451e6b16f231914
uidp3755 <uidp3755@BHL5B1FG.cw01.contiwan.com>
2016-03-15 16:45:20 +0100

    ghdth

--- Fuhrpark/src/Fuhrpark.java
+++ Fuhrpark/src/Fuhrpark.java
@@ -1,5 +1,5 @@
 public class Fuhrpark {
 
 	public static void main(String[] args) {
-		System.out.println("guge ma hallo was geht, Tes2\n");}
+		System.out.println("guge ma hallo ws geht, Tes2\n");}
 }
