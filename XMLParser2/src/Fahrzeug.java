4adb3a840b9c1660024c6acf39d7721b0e79e63a
uidp3755 <uidp3755@BHL5B1FG.cw01.contiwan.com>
2016-03-16 12:16:15 +0100

    Fahrzeug.xml und Fahrzeug.java Attribute angepasst

--- XMLParser2/src/Fahrzeug.java
+++ XMLParser2/src/Fahrzeug.java
@@ -3,9 +3,43 @@ abstract public class Fahrzeug {
 	public static String fahrzeugtyp;
 	public static boolean geliehen;
 	public static String zweck;
-	public static int maxGewicht;
+	public static String kennzeichen;
 	
-	protected Fahrzeug(){ //Konstruktor Fahrzeug
-		
+	public static String getKennzeichen() {
+		return kennzeichen;
+	}
+
+	public static void setKennzeichen(String kennzeichen) {
+		Fahrzeug.kennzeichen = kennzeichen;
+	}
+
+	protected Fahrzeug(String fahrzeugtyp, boolean geliehen, String zweck){ //Konstruktor Fahrzeug
+		this.fahrzeugtyp = fahrzeugtyp;
+		this.geliehen = geliehen;
+		this.zweck = zweck;
+	}
+
+	public static String getFahrzeugtyp() {
+		return fahrzeugtyp;
+	}
+
+	public static void setFahrzeugtyp(String fahrzeugtyp) {
+		Fahrzeug.fahrzeugtyp = fahrzeugtyp;
+	}
+
+	public static boolean isGeliehen() {
+		return geliehen;
+	}
+
+	public static void setGeliehen(boolean geliehen) {
+		Fahrzeug.geliehen = geliehen;
+	}
+
+	public static String getZweck() {
+		return zweck;
+	}
+
+	public static void setZweck(String zweck) {
+		Fahrzeug.zweck = zweck;
 	}
 }
